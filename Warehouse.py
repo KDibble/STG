f56699fb29de4701753b6429da85865d8b75bcb3
Kevin Dibble <kdibble@cs.uml.edu>
2016-11-18 12:52:28 -0500

    Initial setup for holding the box position data. Need to fill out the self linked-list that will hold the space and box information

--- Warehouse.py
+++ Warehouse.py
@@ -1,3 +1,5 @@
+import math
+
 def Create_Warehouse(height, length):
     i = 0
     j = 0
@@ -12,3 +14,34 @@ def Create_Warehouse(height, length):
         j = 0
 
     return warehouse
+
+# Using Feet as only measurment
+class Warehouse(object):
+    shelves = []
+    def __init__(self, l, w, h):
+        self.length = l
+        self.width = w
+        self.height = h
+        self.resetShelves()
+
+    def resetShelves(self):
+        numFloors = math.floor(self.height / 6) # 6' per floor
+        numShelves = numFloors * 2 # 2 shelves per floor
+
+class Shelf(object):
+    def __init__(self, length, width):
+        #How wide are shelves? Let's say they're 60 cm, which is just under 2'
+        self.length = length
+        self.width = width
+
+    
+
+class Box(object):
+    def __init__(self, length, width, height, contents):
+        self.height = length
+        self.width = width
+        self.depth = height
+        self.contents = contents
+
+    def setPosition(self, floor, shelf, distanceFromBeginning):
+        self.position = dict(floor=floor, shelf=shelf, distance=distanceFromBeginning)
